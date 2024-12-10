b3954f7d7f0ede7b604fd420308e76b2ea75b5ef
Jairo Andrades Bueno <jairo.po8@gmail.com>
2024-12-10 11:14:58 +0100

    Modificado

--- src/app.py
+++ src/app.py
@@ -3,18 +3,13 @@ import streamlit as st
 st.title("¡SUMA DE DOS NÚMEROS!")
 
 st.write("Bienvenido/a/e a la app para sumar dos números.")
-st.write("Valores por defecto: 1 y 2")
-
-valor1 = 1
-valor2 = 2
-
 valor1 = st.text_input("Introduce el primer valor:")
 valor2 = st.text_input("Introduce el segundo valor:")
 
-try:
+if valor1 and valor2:
     suma = valor1 + valor2
     st.write("La suma de los valores ", valor1, " y ", valor2, " es: ", suma, "!!")
-except:
+else:
     st.write("La suma no se ha podido realizar, comprueba los valores introducidos :(")
 
 
